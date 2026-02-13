baf1b81150bbc413816c5967b4e8ef78d9b49cc3
Andrés <andresgarcia@tretto.co>
2026-02-12 20:08:20 -0500

    master links

---
+++ links.js
@@ -0,0 +1,7 @@
+function setVH(){
+  const vh = window.innerHeight * 0.01;
+  document.documentElement.style.setProperty('--vh', `${vh}px`);
+}
+
+setVH();
+window.addEventListener('resize', setVH);
